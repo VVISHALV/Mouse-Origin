347e7bfd4e54d8788ac87e3558907e36e9d122f2
VVISHALV <93324607+VVISHALV@users.noreply.github.com>
2022-05-30 08:00:35 +0530

    Add files via upload

--- src/Client.java
+++ src/Client.java
@@ -1,14 +1,20 @@
 import java.net.*;
 import java.io.*;
-
+import java.awt.AWTException;
+import java.awt.Robot;
 public class Client {
-
+static int mouseXaxis=0;
     private Socket socket = null;
     private DataInputStream input = null;
     private static DataOutputStream out = null;
-
+    static Robot robot=null;
     public  Client(String address, int port) {
-
+        try {
+            robot=new Robot();
+        } catch (AWTException e) {
+           
+            e.printStackTrace();
+        }
         try {
             socket = new Socket(address, port);
             System.out.println("Connected");
@@ -25,13 +31,30 @@ public class Client {
        
     }
     public static  void mouseMotionSend (int x,int y){
+
         try {
-            out.writeInt(x);
-            out.writeInt(y);
+            if(x>0){
+                out.writeInt(x);
+                out.writeInt(y);
+                mouseXaxis+=(x-1500);
+               // setMouse(1500, y);
+               // System.out.print(" "+mouseXaxis+" ");
+            }else
+            if(mouseXaxis>0){
+              //  setMouse(1500, y);
+                mouseXaxis-=(1500-x);
+                out.writeInt(x);
+                out.writeInt(y);
+            }
+           
+            
         } catch (IOException e) {
            
             e.printStackTrace();
         }
+    }
+    public static void setMouse(int x, int y) {
+        robot.mouseMove(x, y);
     }
    
 }
